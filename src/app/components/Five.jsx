169e0e795c5eb871807b864494f8eec73c2e897b
azkarullhassan <azkarullhassan7@gmail.com>
2024-06-11 13:24:38 +0500

    first commit

--- src/app/components/Five.jsx
+++ src/app/components/Five.jsx
@@ -5,14 +5,14 @@ const Five = () => {
     <section className='w-full py-56'>
         <main className='mx-auto max-w-[80rem] bg-[#FF6947] rounded-xl pb-6'>
             <div className='pt-9 text-white'>
-                <p className='font-extrabold text-[42px] flex justify-center md:p-0 p-5'>Get Started with AMT MEDIA </p>
+                <p className='font-extrabold text-[42px] flex justify-center md:p-0 p-5'>Lorem ipsum dolor sit amet. </p>
                 <p className='font-extrabold text-[42px] flex justify-center'>Lorem ipsum dolor sit amet.</p>
             </div>
             <div className='flex md:flex-row flex-col justify-center gap-10 p-10 -mb-56 '>
                 <div className='bg-white rounded-xl p-5 shadow-xl'>
                     <h1 className='font-extrabold text-[54px] text-[#FF6947] '>01</h1>
                     <h2 className='font-extrabold text-[24px] '>Lorem ipsum dolor sit.:</h2>
-                    <p className='font-medium text-[18px] pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam consequatur dolorum placeat. rtyui ertyu  This meeting helps us understand your needs, goals, and challenges. It's a great chance to ask questions and see what your brand could achieve with professional video content.</p>
+                    <p className='font-medium text-[18px] pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, sit? dolorum placeat. rtyui ertyu  This meeting helps us understand your needs, goals, and challenges. It's a great chance to ask questions and see what your brand could achieve with professional video content.</p>
 
                 </div>
 
