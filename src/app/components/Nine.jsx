9936c98975809ff5c483a0117514803a402ee84e
azkarullhassan <azkarullhassan7@gmail.com>
2024-12-16 09:40:50 +0500

    first commit

--- src/app/components/Nine.jsx
+++ src/app/components/Nine.jsx
@@ -8,7 +8,7 @@ const Nine = () => {
           <div className='flex md:flex-row flex-col justify-center'>
             <div className='md:w-1/2 p-20'>
                <h1 className='font-semibold text-[40px]'>Lorem ipsum dolor sit amet.</h1>
-               <p className='font-medium text-[18px] pt-6 pb-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, molestias. a powerful digital footprint. Join us at AMT MEDIA, where your aspirations meet our expertise. Your next step? A simple call or email to get the ball rolling.</p>
+               <p className='font-medium text-[18px] pt-6 pb-6'>Lorem ipsum dolor sit AMTR conskfcgh adipisicing iuytr. Maiores, molestias. a powerful digital footprint. Join us at AMT MEDIA, where your aspirations meet our expertise. Your next step? A simple call or email to get the ball rolling.</p>
                <button className='border-[1px] md:px-8 px-3 py-3 text-[#FF6947] rounded-lg text-[16px] bg-white font-bold'>
            Lorem, ipsum.
           </button>
